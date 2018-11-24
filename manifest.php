e2f5b5199d276f1c5275d697c57461460ea7755f
Leander Seige <leander@seige.name>
2018-11-24 23:56:14 +0100

    php manifest echo added

---
+++ manifest.php
@@ -0,0 +1,18 @@
+<?php
+	function decode_gzip( $data )
+    {
+        return gzinflate( substr( $data, 10, -8 ) );
+    }
+
+    if(isset($_GET['m'])) {
+		$data = $_GET['m'];
+       	$json = decode_gzip( utf8_decode( base64_decode($data) ) );
+        if ( is_null( $json ) )
+        {
+            $json = json_decode( $data );
+        }
+        header('Content-Type: application/json');
+        die($json); 
+    }
+	die();
+?>
